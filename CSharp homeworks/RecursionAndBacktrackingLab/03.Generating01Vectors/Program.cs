8e647492a7a47bb9d8e693d60bbbd64345459ae1
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2020-12-15 20:19:22 +0200

    add new homework

---
+++ CSharp homeworks/RecursionAndBacktrackingLab/03.Generating01Vectors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03.Generating01Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var vector = new int[n];
+
+            CreateVector(vector, 0);
+        }
+
+        private static void CreateVector(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join("",vector));
+                return;
+            }
+
+            for (int number = 0; number <= 1; number++)
+            {
+                vector[index] = number;
+                CreateVector(vector, index + 1);
+            }
+        }
+    }
+}
