d5c5c832fadf3a99646af24bb468ed17e61b4580
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2020-12-15 21:31:03 +0200

    add new homework

--- CSharp homeworks/RecursionAndBacktrackingLab/05.FindAllPathsInALabyrinth/Program.cs
+++ CSharp homeworks/RecursionAndBacktrackingLab/05.FindAllPathsInALabyrinth/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05.FindAllPathsInALabyrinth
 {
@@ -6,7 +7,73 @@ namespace _05.FindAllPathsInALabyrinth
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            char[,] lab = new char[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    lab[row, col] = input[col];
+                }
+            }
+            List<char> directions = new List<char>();
+            FindAllPaths(lab, 0, 0, directions, '\0');
+        }
+
+        private static void FindAllPaths(char[,] lab, int row, int col, List<char> directions, char direction)
+        {
+            if (IsOutside(lab, row, col)
+                || IsWall(lab,row,col)
+                || IsVisited(lab,row,col))
+            {
+                return;
+            }
+
+            directions.Add(direction);
+
+            if (IsSolution(lab, row, col))
+            {
+                Console.WriteLine(string.Join("",directions));
+                directions.RemoveAt(directions.Count - 1);
+
+                return;
+            }
+
+            lab[row, col] = 'v';
+
+            FindAllPaths(lab, row + 1, col, directions, 'D');
+            FindAllPaths(lab, row - 1, col, directions, 'U');
+            FindAllPaths(lab, row, col - 1, directions, 'L');
+            FindAllPaths(lab, row, col + 1, directions, 'R');
+            directions.RemoveAt(directions.Count - 1);
+            lab[row, col] = '-';
+        }
+
+        private static bool IsSolution(char[,] lab, in int row, in int col)
+        {
+            return lab[row, col] == 'e';
+
+        }
+
+        private static bool IsVisited(char[,] lab, in int row, in int col)
+        {
+            return lab[row, col] == 'v';
+        }
+
+        private static bool IsWall(char[,] lab, in int row, in int col)
+        {
+            return lab[row, col] == '*';
+        }
+
+        private static bool IsOutside(char[,] lab, int rows, int cols)
+        {
+            return rows < 0 ||
+                   rows >= lab.GetLength(0) ||
+                   cols < 0 ||
+                   cols >= lab.GetLength(1);
         }
     }
 }
