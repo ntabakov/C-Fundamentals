6e71a4082779aaf458d215537208d5f3d17ca92d
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-02-24 11:13:15 +0200

    test

---
+++ CSharp homeworks/RecursionAndCombinatorialProblemsExercise/test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            Console.WriteLine(i+=1);
+        }
+    }
+}
